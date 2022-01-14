76eecea4ee5ae24f61fdeac52f590669afd174d6
FlorentNICOLAS <contact@florentnicolas.com>
2022-01-14 10:17:07 +0100

    ReminderSenderCommand scheduler

--- app/Console/Commands/ReminderSenderCommand.php
+++ app/Console/Commands/ReminderSenderCommand.php
@@ -13,15 +13,11 @@ class ReminderSenderCommand extends Command
 {
     /**
      * The name and signature of the console command.
-     *
-     * @var string
      */
-    protected $signature = 'reminders:send';
+    protected $signature = 'reminders:sendmail';
 
     /**
      * The console command description.
-     *
-     * @var string
      */
     protected $description = 'Send reminders';
 
